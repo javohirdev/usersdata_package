e86bf0e71ef4cc2946885cfe1fe5a163fa052bbd
Javohir Hakimov <57003102+javohirdev@users.noreply.github.com>
2022-06-08 18:17:24 +0500

    main code of npm package

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+// created and exported function
 module.exports = async function getDatas(){
-    const axios = require("axios");
+    const axios = require("axios"); // first install axios, after you should import axios for use it
 
-    await axios.get("https://jsonplaceholder.typicode.com/posts")
+    await axios.get("https://jsonplaceholder.typicode.com/posts") // fetch jsonplaceholder api
     .then((res) => {console.log(res.data);})
     .catch((err) => {console.error(err);})
 }
